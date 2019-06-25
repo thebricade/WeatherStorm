6998eeea4b02c5d528f541fb119b080e49d3ca81
Bri <bs3864@nyu.edu>
2019-06-25 01:08:06 -0400

    basic planting

---
+++ WeatherStorm/Assets/Materials/myFirstShader.shader
@@ -0,0 +1,14 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Unlit/myFirstShader"
+{
+   Properties {
+    _Color ("Color", Color) = (1,1,1)
+}
+ 
+SubShader {
+    Color [_Color]
+    Pass {}
+    }
+      
+}
---
+++ WeatherStorm/Assets/Scripts/BasicPlanting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlanting : MonoBehaviour
+{
+    //public GameObject Ground;
+
+    private Vector3 craftTargetVector;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnMouseDown()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        RaycastHit raycastHit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out raycastHit))
+        {
+            craftTargetVector = raycastHit.point; 
+        }
+
+        GameObject newPlant = Instantiate(Resources.Load<GameObject>("Prefabs/Plant"), craftTargetVector,
+            Quaternion.identity);
+    }
+}
